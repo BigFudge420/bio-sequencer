2b8dff7de634330838e70c16ad2a4085f7435187
BigFudge420 <87513995+BigFudge420@users.noreply.github.com>
2025-12-06 19:48:59 +0000

    Add input component and fetch data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import './App.css'
 
 export default function App(){
-    return
+    return (
+        <div></div>
+    )
 }
---
+++ client/src/pages/Input.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { useState } from "react";
+
+export default function Input({ setSubmitted, setData }) {
+    const [seq, setSeq] = useState('ACTG')
+    const [label, setLabel] = useState('No label')
+    const [seqType, setSeqType] = useState('DNA')
+
+    const API_URL = import.meta.env.VITE_API_URL
+
+    const fetchData = async (seq, label, seqType) => {
+        try {
+            const res = await fetch(`${API_URL}/analyse`, {
+                method: 'POST',
+                headers: {'Content-Type' : 'applications/json'},
+                body : JSON.stringify({
+                    seq : seq,
+                    seq_type : seqType,
+                    label : label
+                })
+            })
+    
+            const data = await res.json()
+            setData(data)
+            setSubmitted(true)
+        }
+        catch (err) {
+            console.error('Fetch failed: ', err)
+        }
+
+    }
+
+    return (
+        <div className="form">
+            <div className="input-container">
+                <label htmlFor="seq">Enter a sequence to analyze</label>
+                <input 
+                    id="seq" 
+                    className="seq-input input" 
+                    type="text"
+                    onChange={() => setSeq(this.value)}
+                />
+            </div>
+            <div className="input-container">
+                <label htmlFor="label">Label the sequence</label>
+                <input 
+                    id="label"
+                    className="label-input input"
+                    type="text" 
+                    onChange={() => setLabel(this.value)}
+                />
+            </div>
+            <div className="input-container">
+                <label htmlFor="seq-type">Enter a sequence type</label>
+                <select 
+                    name="seq-type" 
+                    id="seq-type" 
+                    className="type-input input"
+                    onChange={() => setSeqType(this.value)}
+                >
+                    <option value="DNA">DNA</option>
+                    <option value="RNA">RNA</option>
+                </select>
+            </div>
+            <button 
+                className="submit-btn"
+                onClick={() => fetchData(seq, label, seqType)}
+            >
+                Submit
+            </button>
+        </div>
+    )
+}
