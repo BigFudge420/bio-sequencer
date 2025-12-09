db3b6e635574098f289e49cfece2a718b24affdb
BigFudge420 <87513995+BigFudge420@users.noreply.github.com>
2025-12-10 02:23:50 +0530

    Perform file checks and log

--- server/api/api.py
+++ server/api/api.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, HTTPException, File, UploadFile, Form  # type: ignore
 from fastapi.middleware.cors import CORSMiddleware  # type: ignore
-from logic.bio_seq import BioSeq
+from typing import Optional
+from bio_utils.bio_seq import BioSeq
+from bio_utils.bio_structs import SEQ_TYPES
+import logging
 
 app = FastAPI()
 
@@ -11,13 +14,22 @@ app.add_middleware(
     allow_headers=['*']
 )
 
+logger = logging.getLogger("bioseq_logger")
+
 @app.post('/analyse')
 def analyze(
     file : UploadFile = File(...),
     seq_type: str = Form('DNA') 
     ) -> dict:
 
-    print(f"Received file: {file.filename} of type {seq_type}")
+    if seq_type not in SEQ_TYPES:
+        raise HTTPException(status_code=400, detail=f"Invalid seq_type. Must be one of {SEQ_TYPES}")
+    
+    if not file.filename:
+        raise HTTPException(status_code=400, detail="No file uploaded.")
+
+    logger.info(f"Received file: {file.filename} for analysis as {seq_type}")
+
 
     return {
         "message": "This is a placeholder response from the /analyse endpoint."
