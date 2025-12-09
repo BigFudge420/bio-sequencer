7f88ddb0b33c63056e5224b2e401e1f0ed2619fc
BigFudge420 <87513995+BigFudge420@users.noreply.github.com>
2025-12-09 21:12:20 +0530

    Add sanitize.py

---
+++ server/logic/sanitize.py
@@ -0,0 +1,23 @@
+import re
+import logging
+from typing import Optional
+
+def sanitize(header: Optional[str], logger : Optional[logging.Logger],fallback : str = 'SEQ_01', maxlen : int = 100) -> str:
+    if not header:
+        return fallback
+    
+    s = header.lstrip('> \t')
+    s = re.sub(r'\s+', '_', s)
+    s = re.sub(r'[^A-Za-z0-9_.-]', '_', s)
+    s = re.sub(r'_+', '_', s)
+    s = s[0:maxlen]
+    s = s.strip('_.-')
+    
+    if not s:
+        return fallback
+    
+    if logger:
+        logger.info(f'Raw header: {header}')
+        logger.info(f'Sanitized header to: {s}')
+
+    return s
